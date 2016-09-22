014f6d59ed3e9d8bcd3518f0e3f7991d34fe9903
vitaliyKashuba <k.vitaliy1994@gmail.com>
2016-09-22 23:55:17 +0300

    add method to draw circles work with bug caused by magic of lib

--- src/main/java/berestlabs/GraphCreator.java
+++ src/main/java/berestlabs/GraphCreator.java
@@ -1,5 +1,6 @@
 package berestlabs;
 
+import java.util.ArrayList;
 import java.util.TreeMap;
 import org.knowm.xchart.XYChart;
 import org.knowm.xchart.*;
@@ -14,8 +15,11 @@ import org.knowm.xchart.style.markers.Marker;
  */
 public class GraphCreator 
 {
-    private TreeMap<Integer, Integer> Circles; //contains coords of circles points
+    private TreeMap<Double, Double> circles; //contains coords of circles points
     private XYChart chart;
+    private static double ROUND_PAINTER_STEP = 0.1; //used in circle points calculation
+    ArrayList<Double> xSet = new ArrayList<>();
+    ArrayList<Double> ySet = new ArrayList<>();
     
     /**
      * add coords and circle-border of clas on graph
@@ -43,16 +47,70 @@ public class GraphCreator
         chart.addSeries(iRClass.getName(), xData, yData);
         chart.addSeries(iRClass.getName() + "etalon", x, y);
         
-        addCircle();
+        calculateCircle(etalon, radius);
     }
     
-    private void addCircle()
+    private void calculateCircle(int [] center, double radius)
     {
-        //calculate and add coords of circles
+        double firstY = center[1]-radius;
+        int steps = (int)Math.ceil((radius*2)/ROUND_PAINTER_STEP); // count of points. seems like magic, but works right, no need to test
+        double step = 0;
+        
+        double[] xRound = new double[steps];
+        double[] yRound = new double[steps];
+        
+        for (int i = 0; i < steps; i++, step = step+ROUND_PAINTER_STEP)
+        {
+            double yCord = firstY + step;
+            double xCord = Math.pow( (Math.pow(radius, 2) -  Math.pow(yCord-center[1], 2) ), 0.5) + center[0];
+            
+            xRound[i] = xCord;
+            yRound[i] = yCord;
+            
+            //circles.put(xCord, yCord);
+            xSet.add(xCord);
+            ySet.add(yCord);
+        }
+        
+        halfCircleMirror(xRound, yRound);
+    }
+    
+    /**
+     * mirrors coordinates of half-circle
+     * caused by two solutions in XY-earch equation
+     */
+    private void halfCircleMirror(double xRound[], double yRound[])
+    {
+        double distanceToYAxis = Math.sqrt( Math.pow(xRound[0],2) );
+        double[] xRoundMirror = new double[xRound.length];
+        for(int i = 0; i < xRound.length; i++)
+        {
+            xRoundMirror[i] = xRound[i] * (-1) + distanceToYAxis*2;
+            //circles.put(xRoundMirror[i], yRound[i]);
+            xSet.add(xRoundMirror[i]);
+            ySet.add(yRound[i]);
+        }
+        System.out.println("here!");
+    }
+    
+    /**
+     * called befor show graph
+     */
+    private void addCirclesOnGraph()
+    {
+        /*ArrayList<Double> xSet = new ArrayList<>();
+        ArrayList<Double> ySet = new ArrayList<>();
+        for(double key : circles.keySet())
+        {
+            xSet.add(key);
+            ySet.add(circles.get(key));
+        }*/
+        chart.addSeries("circles", xSet, ySet);
     }
     
     void show()
     {
+        addCirclesOnGraph();
         new SwingWrapper(chart).displayChart();
     }
     
@@ -63,5 +121,7 @@ public class GraphCreator
         chart.getStyler().setChartTitleVisible(false);
         chart.getStyler().setLegendPosition(LegendPosition.InsideSW);
         chart.getStyler().setMarkerSize(16);
+        
+        circles = new TreeMap<>();
     }
 }
