99c078fe3637204df7c9d06a5cbf074bf2cb1b3b
vitaliyKashuba <k.vitaliy1994@gmail.com>
2016-09-26 22:30:28 +0300

    finished with xml out

--- src/main/java/EfectiveTester/EffectiveTester.java
+++ src/main/java/EfectiveTester/EffectiveTester.java
@@ -4,8 +4,14 @@ import static berestlabs.BerestLabs.LEARNING_LIMIT;
 import berestlabs.IRClass;
 import berestlabs.IRExamenator;
 import berestlabs.IRUtil;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 
 /**
@@ -14,50 +20,93 @@ import org.apache.poi.ss.usermodel.Workbook;
 public class EffectiveTester 
 {
     private static int LEARNING_LIMIT = 80;
-    private static Workbook book;
+    private static Workbook book = new HSSFWorkbook();;
     
     public static void main(String[] args) throws IOException 
     {
-        testByRealizations(args, false);
+        testByRealizations(args, true);
         System.out.println("\n\n");
-        testByAttribute(args, false);
+        testByAttribute(args, true);
         System.out.println("\n\n");
-        testByClasses(args, false);
+        testByClasses(args, true);
+        
+        try 
+        {
+            book.write(new FileOutputStream(new File("G:\\__haveNoIdeaWhatImDoingHere\\BerestLabs\\out.xls")));
+            //book.close();
+        } catch (Exception e) 
+        {
+            System.out.println("writing error");
+        }
+            
+    }
+    
+    static void writeToSheet(Sheet sheet, int rownum, String val1, String val2)
+    {
+        Row row = sheet.createRow(rownum); 
+        Cell rl = row.createCell(0);
+        rl.setCellValue(val1);
+        Cell r2 = row.createCell(1);
+        r2.setCellValue(val2);
     }
     
     static void testByRealizations(String[] args, boolean writeToXML) throws IOException
     {
         int realizationLimit = 5;
+        Sheet sheet = book.createSheet("test by realizations");
+        int rw = 0; //for xml
         
         while (realizationLimit < 100)
         {
             double effect = test(args, 2, 0, realizationLimit);
             System.out.println("Learning limit = " + realizationLimit + " effectiveness = "+ effect);
             realizationLimit = realizationLimit + 5;
+            
+            if (writeToXML)
+            {
+                writeToSheet(sheet, rw, String.valueOf(realizationLimit), String.valueOf(effect));
+                rw++;
+            }
         }
     }
     
     static void testByAttribute(String[] args, boolean writeToXML) throws IOException
     {
         int ignoreAttributes = 0;
+        Sheet sheet = book.createSheet("test by attributes");
+        int rw = 0; //for xml
         
         while (ignoreAttributes < 100)
         {
             double efect = test(args, 2, ignoreAttributes, LEARNING_LIMIT);
             System.out.println("Attributes count = " + (100-ignoreAttributes) + " effectiveness = " + efect);
             ignoreAttributes = ignoreAttributes + 5;
+            
+            if (writeToXML)
+            {
+                writeToSheet(sheet, rw, String.valueOf(100-ignoreAttributes), String.valueOf(efect));
+                rw++;
+            }
         }
     }
     
     static void testByClasses(String[] args, boolean writeToXML) throws IOException
     {
         int classCount = 2;
+        Sheet sheet = book.createSheet("test by classes");
+        int rw = 0; //for xml
         
         while (classCount <= args.length)
         {
             double efect = test(args, classCount, 0, LEARNING_LIMIT);
             System.out.println("Class count = " + classCount + " effectiveness = " + efect);
             classCount++;
+            
+            if (writeToXML)
+            {
+                writeToSheet(sheet, rw, String.valueOf(classCount), String.valueOf(efect));
+                rw++;
+            }
         }
     }
     
